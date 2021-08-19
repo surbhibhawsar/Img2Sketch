e7afb3ae9bd513989e6bca6c4648ef1190b0ca3d
surbhi bhawsar <68633698+surbhibhawsar@users.noreply.github.com>
2021-08-19 16:17:02 +0530

    Add files via upload

---
+++ sketch.py
@@ -0,0 +1,18 @@
+import cv2
+img_location ='surbhi.jpg'
+
+img = cv2.imread(img_location)
+
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+inverted_gray_image = 255- gray_image
+
+blur_image = cv2.GaussianBlur(inverted_gray_image,(21,21), 0)
+
+inverted_blur_image = 255- blur_image
+
+pencil_image = cv2.divide(gray_image, inverted_blur_image, scale=256.0)
+
+cv2.imshow('Original Image',img)
+cv2.imshow('New Image',pencil_image)
+cv2.waitKey(0)
